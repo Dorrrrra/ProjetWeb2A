4ccc7bf5577229327d78f9cfe4655677d37fe1ce
DoraSioud <dora.sioud15@gmail.com>
2024-02-19 23:10:17 +0100

    php

--- inscription.js
+++ inscription.js
@@ -15,6 +15,10 @@ function validerDateOfBirth()
 
 var formElement = document.getElementById("form");
 var nameElement = document.getElementById("nom");
+var prenomElement = document.getElementById("prenom");
+var emailElement = document.getElementById("email");
+var telephoneElement = document.getElementById("telephone");
+var emailElement = document.getElementById("
 formElement.addEventListener("submit",function(event)){
     event.preventDefault();
     validateName()
@@ -34,3 +38,19 @@ function validateName(){
     }
 }
 
+function validateEmail(){
+    var emailError = document.getElementById("emailError");
+    var emailCorrect = document.getElementById("emailCorrect");
+    var emailValue = emailElement.value;
+    var mailPattern = /^[a-z A-Z 0-9 _%.+-]@esprit.tn$/;
+
+    if(!emailValue.match(mailPattern))
+    {
+        emailError.innerHTML ="Mail Incorrect";
+        emailCorrect.innerHTML="";
+    }
+    else{
+        emailCorrect.innerHTML = "Mail Correct";
+        emailError.innerHTML="";
+    }
+}
